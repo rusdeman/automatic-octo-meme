e3357ea1331f1bca43a6b4fb1a47040e7f0243e6
rusdeman <rusdeman@gmail.com>
2021-07-08 16:40:18 +0300

    add admin account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         User::factory(10)->create();
+//        User::factory()->create();
+
         $author = Author::factory()
             ->create();
 
